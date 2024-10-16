51f0fcfd67ba53d735cfbde1bc84d6aac0f07e41
oweslley03 <wesleymoraeswv03@hotmail.com>
2024-10-16 11:49:14 -0300

    implemented token schematic for sending document

--- backend-application/src/aluno/aluno.service.ts
+++ backend-application/src/aluno/aluno.service.ts
@@ -1,11 +1,27 @@
 import { Injectable } from '@nestjs/common';
 import { CreateAlunoDto } from './dto/create-aluno.dto';
 import { Aluno } from './models/aluno.model';
+import { sign } from 'jsonwebtoken';
 
 type ResponseBody = {
   status: boolean;
   message: string;
+  token?: string;
 };
+
+async function generateToken(aluno_id: string): Promise<string> {
+  const JWT_SECRET_KEY = process.env.JWT_SECRET_KEY;
+  if (!JWT_SECRET_KEY)
+    throw new Error(
+      'JWT_SECRET_KEY is not defined in the environment variables',
+    );
+
+  const payload = { aluno_id };
+  const token = sign(payload, JWT_SECRET_KEY, {
+    expiresIn: '1h',
+  });
+  return token;
+}
 
 @Injectable()
 export class AlunoService {
@@ -23,11 +39,14 @@ export class AlunoService {
       return response;
     }
 
-    await Aluno.query().insert(createAlunoDto);
+    const newAluno = await Aluno.query().insert(createAlunoDto);
+
+    const token = await generateToken(newAluno.id.toString());
 
     const response: ResponseBody = {
       status: true,
       message: 'user created sucefully',
+      token: token,
     };
     return response;
   }
