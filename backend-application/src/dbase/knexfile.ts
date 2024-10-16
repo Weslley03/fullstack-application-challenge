d0f878850c8263842199655ce28d0fa8e22e73ab
oweslley03 <wesleymoraeswv03@hotmail.com>
2024-10-16 11:01:51 -0300

    removing development redundancy regarding the connection to the database

--- backend-application/src/dbase/knexfile.ts
+++ backend-application/src/dbase/knexfile.ts
@@ -3,12 +3,12 @@ import * as dotenv from 'dotenv';
 dotenv.config();
 
 const config: Knex.Config = {
-  client: process.env.DB_CLIENT || 'mysql2',
+  client: process.env.DB_CLIENT,
   connection: {
-    host: process.env.DB_HOST || 'localhost',
-    user: process.env.DB_USER || 'root',
-    password: process.env.DB_PASSWORD || 'weslley03',
-    database: process.env.DB_DATABASE_NAME || 'educational-institution',
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE_NAME,
   },
 };
 export default config;
