7577a0b5c26fa95c23e03039d026852b3819aa5f
oweslley03 <wesleymoraeswv03@hotmail.com>
2024-10-16 15:33:58 -0300

    implemented migrate for document table

--- backend-application/src/dbase/migrations/20241015211605_create_alunos_table.ts
+++ backend-application/src/dbase/migrations/20241015211605_create_alunos_table.ts
@@ -1,7 +1,8 @@
 import type { Knex } from 'knex';
 
 export async function up(knex: Knex): Promise<void> {
-  return knex.schema.createTable('aluno', (table) => {
+  await knex.schema
+  .createTable('aluno', (table) => {
     table.increments('id').primary();
     table.string('aluno_nome').notNullable();
     table.string('aluno_email').notNullable().unique();
@@ -10,10 +11,41 @@ export async function up(knex: Knex): Promise<void> {
     table.date('aluno_nascimento').notNullable();
     table.string('aluno_CEP').notNullable();
     table.string('documento_number').notNullable();
-    table.string('documento_id');
+    table.integer('documento_id').unsigned();
+  });
+
+  await knex.schema
+  .createTable('documento', (table) => {
+    table.increments('id').primary();
+    table.integer('aluno_id').unsigned().notNullable();
+    table.string('documento_desc').notNullable();
+    table.string('documento_image').notNullable();
+  });
+
+  await knex.schema
+  .alterTable('aluno', (table) => {
+    table.foreign('documento_id').references('id').inTable('documento').onDelete('SET NULL');
+  });
+
+  await knex.schema
+  .alterTable('documento', (table) => {
+    table.foreign('aluno_id').references('id').inTable('aluno').onDelete('CASCADE');
   });
 }
 
 export async function down(knex: Knex): Promise<void> {
-  return knex.schema.dropTable('aluno');
+  
+  await knex.schema.alterTable('documento', (table) => {
+    table.dropForeign(['aluno_id']);
+  });
+
+  await knex.schema.alterTable('aluno', (table) => {
+    table.dropForeign(['documento_id']);
+  });
+  
+  await knex.schema
+  .dropTable('aluno');
+
+  await knex.schema
+  .dropTable('documento');
 }
