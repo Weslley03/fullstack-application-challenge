eb2c60eea30cc5bd21e196d3c51f5c3592e727dd
oweslley03 <wesleymoraeswv03@hotmail.com>
2024-10-16 23:21:14 -0300

    downloaded dependencies like bootstrap-vue, adjusted screen and default route.

--- frontend-application/nuxt.config.ts
+++ frontend-application/nuxt.config.ts
@@ -1,5 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
-  devtools: { enabled: true }
-})
+  devtools: { enabled: true },
+  css: [
+    'bootstrap/dist/css/bootstrap.css',
+    'bootstrap-vue-3/dist/bootstrap-vue-3.css'
+  ],
+  build: {
+    transpile: [ 'bootstrap-vue-3' ]
+  },
+  plugins: [
+    { src: './plugins/bootstrap-vue-3.js', mode: 'client' }
+  ]
+});
---
+++ frontend-application/pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="d-flex justify-center mt-2">
+    <div class="text-center">
+      <h1> Seu caminho para o sucesso começa aqui. Escolha seu curso! </h1>
+    </div>
+  </div>
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+.mt-5 {
+  margin-top: 3rem;
+}
+</style>
