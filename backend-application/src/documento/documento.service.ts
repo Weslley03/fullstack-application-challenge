ce3232838818fab893d2ce4ce6802c14962892eb
oweslley03 <wesleymoraeswv03@hotmail.com>
2024-10-18 01:05:49 -0300

    I removed the verifyToken from just one entity, so that it can be used by anyone

--- backend-application/src/documento/documento.service.ts
+++ backend-application/src/documento/documento.service.ts
@@ -1,44 +1,16 @@
 import { Injectable } from '@nestjs/common';
-import { JwtPayload, verify } from 'jsonwebtoken';
 import { CreateDocumentoDto } from './dto/create-documento.dto';
 import { Aluno } from 'src/aluno/models/aluno.model';
 import { Documento } from './models/documento.model';
-import * as dotenv from 'dotenv';
-dotenv.config();
 
 type ResponseBody = {
   status: boolean;
   message: string;
 }
 
-async function verifyToken(token: string): Promise<number> {
-  const JWT_SECRET_KEY = process.env.JWT_SECRET_KEY;
-  if (!JWT_SECRET_KEY) {
-    throw new Error('JWT_SECRET_KEY is not defined in the environment variables');
-  }
-  try {
-    const trimmedToken = token.trim();
-    const decoded = verify(trimmedToken, JWT_SECRET_KEY) as JwtPayload;
-    const aluno_id = decoded.aluno_id;
-    if (!aluno_id) {
-      throw new Error('aluno_id is missing in the token');
-    };
-    const alunoIdAsNumber = parseInt(aluno_id, 10);
-    if (isNaN(alunoIdAsNumber)) {
-      throw new Error('aluno_id is not a valid number');
-    };
-    return alunoIdAsNumber;
-  } catch (error) {
-    console.error('token verification failed:', error);
-    throw new Error('invalid token');
-  }
-}
-
 @Injectable()
 export class DocumentoService {
-  async createDocumento(createDocumentoDto: CreateDocumentoDto, token: string ): Promise<ResponseBody> {
-    const aluno_id = await verifyToken(token);
-
+  async createDocumento(createDocumentoDto: CreateDocumentoDto, aluno_id: number ): Promise<ResponseBody> {
     const data = {
       ...createDocumentoDto,
       aluno_id: aluno_id
--- backend-application/src/matricula/matricula.module.ts
+++ backend-application/src/matricula/matricula.module.ts
@@ -2,9 +2,10 @@ import { Module } from "@nestjs/common";
 import { DatabaseModule } from "src/dbase/database.module";
 import { MatriculaController } from "./matricula.controller";
 import { MatriculaService } from "./matricula.service";
+import { AuthModule } from "src/auth/auth.module";
 
 @Module({
-  imports: [DatabaseModule],
+  imports: [DatabaseModule, AuthModule],
   controllers: [MatriculaController],
   providers: [MatriculaService],
   exports: [MatriculaService]
