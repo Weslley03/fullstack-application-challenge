54f8e8815b2ed124d0f12c8e437b92a834060f9e
oweslley03 <wesleymoraeswv03@hotmail.com>
2024-10-18 14:48:05 -0300

    CORS fix done, waiting for front end requests in prod

--- backend-application/src/main.ts
+++ backend-application/src/main.ts
@@ -5,7 +5,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:3000',
+    origin: 'https://fullstack-application-challenge.onrender.com',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
     allowedHeaders: 'Content-Type, Authorization',
