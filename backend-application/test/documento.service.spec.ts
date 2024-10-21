957b08a230a4f7d191b281232f9c4141017c4a6c
oweslley03 <wesleymoraeswv03@hotmail.com>
2024-10-21 11:42:28 -0300

    added error case in documento.service unit test

--- backend-application/test/documento.service.spec.ts
+++ backend-application/test/documento.service.spec.ts
@@ -1,3 +1,4 @@
+import { Patch } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
 import { Aluno } from 'src/aluno/models/aluno.model';
 import { DocumentoService } from 'src/documento/documento.service';
@@ -9,19 +10,25 @@ jest.mock('../src/aluno/models/aluno.model');
 
 describe('DocumentService', () => {
   let service: DocumentoService;
+  let originalConsoleError;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [DocumentoService],
     }).compile();
     service = module.get<DocumentoService>(DocumentoService);
+
+    originalConsoleError = console.error;
+    console.error = jest.fn(); //mocka os console.error pra não logar nos testes
   });
 
   afterAll(() => {
     jest.clearAllMocks();
+    console.error = originalConsoleError;
   });
 
   describe('createDocumento', () => {
+    
     it('should create a document and update the aluno with document ID', async () => {
       const createDocumentoDto: CreateDocumentoDto = {
         documento_desc: 'CNH',
@@ -63,5 +70,44 @@ describe('DocumentService', () => {
         documento_id: newDocument.id.toString(),
       });
     });
+
+    it('should return an error if updating aluno with document ID fails', async () => {
+      const createDocumentoDto: CreateDocumentoDto = {
+        documento_desc: 'CNH',
+        documento_image: 'base64-png-img-simulate',
+      };
+      const aluno_id = 1;
+
+
+      const newDocument = { id: 1, ...createDocumentoDto, aluno_id };
+
+      (Documento.query as jest.Mock).mockReturnValue({
+        insert: jest.fn().mockResolvedValueOnce(newDocument),
+      });
+
+      const mockFindById = jest.fn().mockReturnValue({
+        patch: jest.fn().mockRejectedValueOnce(new Error('Database error')),
+      });
+
+      (Aluno.query as jest.Mock).mockReturnValue({
+        findById: mockFindById, 
+      });
+
+      const result = await service.createDocumento(createDocumentoDto, aluno_id);
+
+      expect(result).toEqual({
+        status: false,
+        message: 'document created error',
+      });
+
+      expect(Documento.query().insert).toHaveBeenCalledWith({
+        ...createDocumentoDto,
+        aluno_id,
+      });
+
+      expect(Aluno.query().findById).toHaveBeenCalledWith(aluno_id)
+      expect(Aluno.query().findById(aluno_id).patch).toHaveBeenCalled()
+    });
+
   });
 });
